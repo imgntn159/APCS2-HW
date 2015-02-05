b3bb0bffe12094c9c69bb55298186b2f89413800
Kaizen <imgtn159@gmail.com>
2015-02-05 18:56:18 -0500

    Added gitignore, added fib function

--- 01Recursion/Recursion.java
+++ 01Recursion/Recursion.java
@@ -4,25 +4,34 @@ public class Recursion{
 	}
 	
 	public int fact(int n){
-	    if (n<0){
-	        throw new IllegalArgumentException();
-	    }
+		if (n<0){
+			throw new IllegalArgumentException();
+		}
 		return facthelper(n, n-1);
 	}
 	public int facthelper(int a, int b){
-	    int c = a * b;
-	    if (b == 1){
-	        return c;
-	    }else{
-	        return facthelper(c, b-1);
-	    }
+		int c = a * b;
+		if (b == 1){
+			return c;
+		}else{
+			return facthelper(c, b-1);
+		}
 	}
 	
 	public int fib(int n){
-		return 1;
+		if (n<0){
+			throw new IllegalArgumentException();
+		}
+		if (n == 0){
+			return 0;
+		}
+		if (n == 1 || n == 2){
+			return 1;
+		}
+		return fib(n-1) + fib(n-2);
 	}
 	public int fibhelper(int n){
-	    return 3;
+		return 3;
 	}
 	
 	public double sqrt(double n){
--- 01Recursion/Tester.java
+++ 01Recursion/Tester.java
@@ -2,6 +2,7 @@ public class Tester{
     public static void main(String[]args){
         Recursion a = new Recursion();
         System.out.println(a.fact(4));
-        System.out.println(a.fact(-2));
+        //System.out.println(a.fact(-2));
+        System.out.println(a.fib(6));
     }
 }
