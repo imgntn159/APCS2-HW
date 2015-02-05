3a998e7bd6cce9029b8aa4650c8f6f881e2f31d2
Kaizen Castanos <imgtn159@gmail.com>
2015-02-05 14:58:50 +0000

    Added tester class, will add git ignore for .class files later, factorial is done

---
+++ 01Recursion/Tester.java
@@ -0,0 +1,7 @@
+public class Tester{
+    public static void main(String[]args){
+        Recursion a = new Recursion();
+        System.out.println(a.fact(4));
+        System.out.println(a.fact(-2));
+    }
+}
