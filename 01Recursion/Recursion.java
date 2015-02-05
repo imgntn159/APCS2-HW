b05f666f585dbdb67429e7786060cc143b9ef87a
Kaizen Castanos <imgtn159@gmail.com>
2015-02-05 14:59:49 +0000

    Recursion.java changes weren't committed, woops

--- 01Recursion/Recursion.java
+++ 01Recursion/Recursion.java
@@ -1,13 +1,30 @@
 public class Recursion{
 	public String name(){
-		return name;
+		return "Castanos, Kaizen Lorenzo";
 	}
+	
 	public int fact(int n){
-		return 0;
+	    if (n<0){
+	        throw new IllegalArgumentException();
+	    }
+		return facthelper(n, n-1);
 	}
+	public int facthelper(int a, int b){
+	    int c = a * b;
+	    if (b == 1){
+	        return c;
+	    }else{
+	        return facthelper(c, b-1);
+	    }
+	}
+	
 	public int fib(int n){
 		return 1;
 	}
+	public int fibhelper(int n){
+	    return 3;
+	}
+	
 	public double sqrt(double n){
 		return 2;
 	}
