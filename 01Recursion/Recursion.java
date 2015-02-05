57e8e15ef8f8b7aa88c4398b0cbce2ea2637c27a
Kaizen <imgtn159@gmail.com>
2015-02-04 20:14:06 -0500

    Homework Framework (rhymez)

---
+++ 01Recursion/Recursion.java
@@ -0,0 +1,14 @@
+public class Recursion{
+	public String name(){
+		return name;
+	}
+	public int fact(int n){
+		return 0;
+	}
+	public int fib(int n){
+		return 1;
+	}
+	public double sqrt(double n){
+		return 2;
+	}
+}
