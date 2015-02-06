d170e0f8c92b9e5d55f3e2bd836597ee607bf2ab
Kaizen <imgtn159@gmail.com>
2015-02-06 18:20:20 -0500

    .

--- 01Recursion/Tester.java
+++
@@ -1,8 +0,0 @@
-public class Tester{
-    public static void main(String[]args){
-        Recursion a = new Recursion();
-        System.out.println(a.fact(4));
-        //System.out.println(a.fact(-2));
-        System.out.println(a.fib(6));
-    }
-}
