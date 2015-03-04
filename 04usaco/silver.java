401f01db9d7db8f3bc9b873e59ccfcbbf7976996
Kaizen <imgtn159@gmail.com>
2015-03-03 21:07:57 -0500

    Silver compiles
    silver.java compiles HOWEVER it's return zero, so either the timer is
    messed up or something...not sure what I did, but the if statement isn't
    firing in findH

--- 04usaco/silver.java
+++ 04usaco/silver.java
@@ -1,24 +1,50 @@
 public class silver{
-    private int[][] land = new int[3][3];
-    private int depth = 0;
-    public silver(int[][] l, int d){
+    private char[][] land;
+    private int length,sX,sY,eX,eY;
+    public silver(char[][] l,int len,int sX,int sY,int eX,int eY){
         land = l;
-        depth = d;
+        length = len;
+        this.sX = sX-1;
+        this.sY = sY-1;
+        this.eX = eX-1;
+        this.eY = eY-1;
+    }
+    public int find(){
+        return findH(0,sX,sY);
+    }
+    public int findH(int timer,int x,int y){
+        if (timer >= length 
+            || x > land.length-1 
+            || y > land[0].length-1 
+            || x < 0 || y < 0
+            || land[x][y] == '*'){
+            return 0;
+        }
+        if (timer == length && x == eX && y == eY){
+            return 1;
+        }
+        return
+        findH(timer + 1,x+1,y) +
+        findH(timer + 1,x-1,y) +
+        findH(timer + 1,x,y+1) +
+        findH(timer + 1,x,y-1);
     }
 
 
     /*Driver*/
     public static void main(String[]args){
-        int[][] thingy = new int[][]{
-            { 28, 25, 20, 32, 34, 36 },
-            { 27, 25, 20, 20, 30, 34 },
-            { 24, 20, 20, 20, 20, 30 },
-            { 20, 20, 14, 14, 20, 20 }
+        char[][] thingy = new char[][]{
+            { '.', '.', '.', '*', '.' },
+            { '.', '.', '.', '*', '.' },
+            { '.', '.', '.', '.', '.' },
+            { '.', '.', '.', '.', '.' }
         };
-        //row 1:  28 25 20 32 34 36
-        //row 2:  27 25 20 20 30 34
-        //row 3:  24 20 20 20 20 30
-        //row 4:  20 20 14 14 20 20
+        //row 1:  ...*.
+        //row 2:  ...*.
+        //row 3:  .....
+        //row 4:  .....
+        silver a = new silver(thingy,6,1,3,1,5);
+        System.out.println(a.find());
     }
  
 }
