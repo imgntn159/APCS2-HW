ab9d66e7770b02ae89ab226d3fa129dab947a62d
Kaizen <imgtn159@gmail.com>
2015-03-12 13:14:02 -0400

    added get(index) and toString

--- 07linkedlists/MyLinkedList.java
+++ 07linkedlists/MyLinkedList.java
@@ -3,4 +3,29 @@ public class MyLinkedList /*implements List?*/{
     public LNode getHead(){
 	return head;
     }
+    public string toString(){
+	if(head.getVal() == null){
+	    return "[ ]";
+	}
+	String ans = "[";
+	LNode temp = head;
+	while(temp.getNext() != null){
+	    ans += "" + temp.getVal() + ",";
+	    temp = temp.getNext();
+	}
+	ans += "" + temp.getVal() + "]";
+	return ans;
+    }
+    public int get(int index){
+	LNode temp = head;
+	for (int i = 0;i < index;i++){
+	    try{
+		temp = temp.getNext();
+	    }catch(IndexOutOfboundsException e){
+		System.out.println("You suck, pick a better index");
+		return;
+	    }
+	}
+	return temp.getVal();
+    }
 }
