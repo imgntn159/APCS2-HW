7f72a2f0d1634229c411a46feb465ff0f6a35366
Kaizen <imgtn159@gmail.com>
2015-03-29 18:08:45 -0400

    Add/get methods done
    they've been done for a while, I just forgot to push them

--- 10deque/MyDeque.java
+++ 10deque/MyDeque.java
@@ -6,43 +6,64 @@ public class MyDeque<T> {
   private int head,tail,size;
 
   public MyDeque() {
-    list = (T[]) (new Object[10]);
+    list = (T[]) new Object[10];
     head = 0;
     tail = 9;
     size = 0;
   }
 
-  public boolean add(T item) {
-    return true;
-  }
-
-  public void addFirst(T item) {
+  public void resize(){
     return;
   }
 
-  public void addLast(T item) {
+  public void addFirst(T item){
+    //resize();
+    head--;
+    if(head < 0){
+      head += list.length;
+    }
+    list[head] = item;
+    size += 1;
+  }
+  public void addLast(T item){
+    //resize();
+    tail++;
+    if(tail != 0 && tail >= list.length){
+      tail -= list.length;
+    }
+    list[tail] = item;
+    size += 1;
+  }
+
+  public void removeFirst(T item){
+    return;
+  }
+  public void removeLast(T item){
     return;
   }
 
-  public T getFirst() {
+  public T getFirst(){
     if (size == 0) {
       throw new NoSuchElementException();
     }
     return list[head];
   }
 
-  public T getLast() {
+  public T getLast(){
     if (size == 0) {
       throw new NoSuchElementException();
     }
     return list[tail];
   }
 
-  public int size() {
+  public int size(){
     return size;
   }
 
-  public String toString() {
-    return "EMPTY";
+  public static void main(String[]args){
+    MyDeque<Integer> D = new MyDeque<Integer>();
+    D.addFirst(1);
+    D.addLast(2);
+    System.out.println(D.getFirst());
   }
 }
