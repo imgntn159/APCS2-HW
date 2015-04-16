3cb5f0a7006fb37f8e593359056f63933d87a14e
Kaizen <imgtn159@gmail.com>
2015-04-16 13:12:41 -0400

    Added mazes to test, Maze compiles without error Also still have to work on the actual main function

--- 11BFS/Maze.java
+++ 11BFS/Maze.java
@@ -125,36 +125,36 @@ public class Maze{
     }
     
     private boolean solve1(boolean animate, boolean mode){
-	    Frontier rest = new Frontier(mode); //true then q
-	    Point start = new Point(startx,starty);
+	Frontier rest = new Frontier(mode); //true then q
+	Point start = new Point(startx,starty);
 
-	    rest.add(start);//put the start into the Frontier 
+	rest.add(start);//put the start into the Frontier 
 		
-	    boolean solved = false;
-	    while(!solved && rest.hasNext()){
-		if(animate && !solved){
-		    System.out.println(toString(true));
-		}
+	boolean solved = false;
+	while(!solved && rest.hasNext()){
+	    if(animate && !solved){
+		System.out.println(toString(true));
+	    }
 		    //get the top
-		Point next = rest.remove();
+	    Point next = rest.remove();
 		    //check if solved
-		if(maze[next.getX()][next.getY()]=='E'){
+	    if(maze[next.getX()][next.getY()]=='E'){
 			//solved!
-		    solved = true;
+		solved = true;
 		    //addCoordinatesToSolutionArray(next);
-
-			//my point class has a reference to previous points, so the solution will be determined from the final point
-
-		}else{
-			//not solved, so add neighbors to Frontier and mark the floor with x.
-		    maze[next.getX()][next.getY()]='x';
-		    for(Point p : getNeighbors(next)){
-			    rest.add(p);
-		    }
-
+	    }else{
+		    //not solved, so add neighbors to Frontier and mark the floor with x.
+		maze[next.getX()][next.getY()]='x';
+		for(Point p : getNeighbors(next)){
+		    rest.add(p);
 		}
 	    }
-	    return solved;
 	}
+	return solved;
+    }
+    
+    public static void main(String[]args){
+	Maze thing = new Maze(args[0]);
+    }
 }
 
