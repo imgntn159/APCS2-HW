b8043f098163bab6265f4ab9b2b7fcb49d82c811
Kaizen <imgtn159@gmail.com>
2015-05-12 08:24:54 -0400

    MyHeap skeleton

---
+++ 13heaps/MyHeap.java
@@ -0,0 +1,22 @@
+public class MyHeap{
+	private boolean isMax;
+	private heapNode root;
+	public MyHeap(){
+		isMax = true;
+	}
+	public MyHead(boolean thing){
+		isMax = thing;
+	}
+	public String toString(){
+		return "hi!";
+	}
+	public int remove(){
+		return 1;
+	}
+	public void add(int){
+		System.out.println("Not done yet");
+	}
+	public int peek(){
+		return 1;
+	}
+}
